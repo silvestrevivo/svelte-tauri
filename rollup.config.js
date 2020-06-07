21d34f14d817d65509a5e6d9cec6a1d7a20ea29f
silvestrevivo <silvestrevivomillan@gmail.com>
2020-06-07 12:25:31 +0200

    readme.md updated, code cleaned up

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,6 @@
 // import svelte from 'rollup-plugin-svelte';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-//import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
 import babel from 'rollup-plugin-babel';
 import svelte from 'rollup-plugin-svelte-hot';
@@ -51,13 +50,6 @@ export default {
 		// NOTE during dev, we're now gonna serve via rollup-plugin-hot -- it's
 		// easier for simple cases and more stable against Rollup restarts
 		//
-		// In dev mode, call `npm run start` once
-		// the bundle has been generated
-		// !production && serve(),
-
-		// Watch the `public` directory and refresh the
-		// browser on changes when not in production
-		// !production && livereload('public'),
 
 		// If we're building for production (npm run build
 		// instead of npm run dev), minify
@@ -77,20 +69,3 @@ export default {
 		clearScreen: false
 	}
 };
-
-// function serve() {
-// 	let started = false;
-//
-// 	return {
-// 		writeBundle() {
-// 			if (!started) {
-// 				started = true;
-//
-// 				require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
-// 					stdio: ['ignore', 'inherit', 'inherit'],
-// 					shell: true
-// 				});
-// 			}
-// 		}
-// 	};
-// }
