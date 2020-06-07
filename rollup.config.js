da7f320e71628955c9724756f5e753e580e5ef36
silvestrevivo <silvestrevivomillan@gmail.com>
2020-06-07 12:35:07 +0200

    cleaning up code and packages

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,3 @@
-// import svelte from 'rollup-plugin-svelte';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import { terser } from 'rollup-plugin-terser';
